5910b3bc13c4b7bec1fde66525766eed24efeb8c
Mohammed Adem <mohjj46@email.com>
2024-02-27 23:14:46 -0600

    fixed refractor

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -3,11 +3,12 @@ module GildedRose
     attr_reader :quality, :days_remaining
     def initialize(quality, days_remaining)
       @quality, @days_remaining = quality, days_remaining
-  end
+  
+    end
     def tick 
 
     end
-end
+  end
   class Normal < Item
     def tick
       @days_remaining -= 1 
@@ -16,6 +17,7 @@ end
       @quality -= 1 if @days_remaining <= 0
     end
   end
+
   class Brie < Item
     def tick
       @days_remaining -= 1
@@ -38,12 +40,11 @@ end
   SPECIALIZED_CLASSES = {
     'normal'                                    => Normal,
     'Aged Brie'                                 => Brie,
-    'Backstage passes to a TAFKAL80ETC concert' => Backstage
-  }
-    def self.for(name, quality, days_remaining)
+    'Backstage passes to a TAFKAL80ETC concert' => Backstage }
+  def self.for(name, quality, days_remaining)
       (SPECIALIZED_CLASSES[name] || DEFAULT_CLASS).new(quality, days_remaining)
-    end
-    end
+  end
+end
 
 
 
