6de797a1603f0bc43d22969232dce5f7b9aae58f
Mohammed Adem <mohjj46@email.com>
2024-02-27 22:02:28 -0600

    Backstage refractor

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -4,8 +4,26 @@ module GildedRose
     def initialize(quality, days_remaining)
       @quality, @days_remaining = quality, days_remaining
   end
+    def tick 
 
+    end
 end
+  class Normal < Item
+    def tick
+      @days_remaining -= 1 
+      return if@quality == 0
+      @quality -= 1
+      @quality -= 1 if @days_remaining <= 0
+    end
+  end
+  class Brie < Item
+    def tick
+      @days_remaining -= 1
+      return if @quality >= 50
+      @quality += 1
+      @quality += if @days_remaining < 0
+    end
+  end
 
 
 
