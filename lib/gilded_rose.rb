d396465e36db9026da07d25e23877359c5f46be5
Mohammed Adem <mohjj46@email.com>
2024-02-27 22:11:18 -0600

    self refractor

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -24,6 +24,26 @@ end
       @quality += if @days_remaining < 0
     end
   end
+  class Backstage < Item
+    def tick
+      @days_remaining -= 1
+      return               if @quality >= 50
+      return @quality = 0  if @days_remaining < 0
+      @quality  += 1
+      @quality  += 1       if @days_remaining < 10
+      @quality  += 1       if @days_remaining < 5
+    end
+  end
+  DEFAULT_CLASS = Item
+  SPECIALIZED_CLASSES = {
+    'normal'                                    => Normal,
+    'Aged Brie'                                 => Brie,
+    'Backstage passes to a TAFKAL80ETC concert' => Backstage
+  }
+    def self.for(name, quality, days_remaining)
+      (SPECIALIZED_CLASSES[name] || DEFAULT_CLASS).new(quality, days_remaining)
+    end
+    end
 
 
 
